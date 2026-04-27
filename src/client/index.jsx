5a889ba4625f9b638af2217049162e651dc6194d
HtetHtetKyaw05 <HtetHtetKyaw05@users.noreply.github.com>
2026-04-27 17:35:51 +0900

    Initial commit from W3Schools

---
+++ src/client/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import HabitTracker from './HabitTracker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HabitTracker />} />
+            <Route path='*' element={<main style={{ padding: '1rem' }}>
+                  <p>There&apos;s nothing here! </p>
+                </main>
+            }/>
+          </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('contents'),
+);
